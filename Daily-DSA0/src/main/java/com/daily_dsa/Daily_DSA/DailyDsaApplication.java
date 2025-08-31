12a4bcb7341bdd08fe74b6c3117922a89a447dcf
Mr. Mohit <mohit.rathore@gmail.com>
2025-09-01 01:03:13 +0530

    two pointer techniques question added

---
+++ Daily-DSA0/src/main/java/com/daily_dsa/Daily_DSA/DailyDsaApplication.java
@@ -0,0 +1,13 @@
+package com.daily_dsa.Daily_DSA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyDsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyDsaApplication.class, args);
+	}
+
+}
---
+++ Daily-DSA0/src/main/java/com/daily_dsa/Daily_DSA/two_pointers_techniques/opposite_end/LC167_TwoSumII.java
@@ -0,0 +1,31 @@
+package com.daily_dsa.Daily_DSA.two_pointers_techniques.opposite_end;
+
+import java.util.Arrays;
+
+public class LC167_TwoSumII {
+
+    public static void main(String[] args) {
+        //test method
+        System.out.println("output -> "+ Arrays.toString(twoSum(new int[]{2, 7, 11, 15},9)));
+        System.out.println("output -> "+ Arrays.toString(twoSum(new int[]{2, 7,9, 11, 15},18)));
+        System.out.println("output -> "+ Arrays.toString(twoSum(new int[]{2, 7,8, 11, 15},17)));
+        System.out.println("output -> "+ Arrays.toString(twoSum(new int[]{2, 7,10, 11, 15},25)));
+    }
+    public static int[] twoSum(int[] numbers, int target) {
+        int start = 0;
+        int end = numbers.length - 1;
+
+        while (start < end) {
+            int sum = numbers[start] + numbers[end];
+            if (sum == target) {
+                return new int[] { start + 1, end + 1 };
+            } else if (sum < target) {
+                start++;
+            } else {
+                end--;
+            }
+        }
+
+        return new int[] { -1, -1 };
+    }
+}
